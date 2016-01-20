214ced7c47ba8ce498aaf042e89232bf75953f33
A.Y. Siu <aysiu@users.noreply.github.com>
2016-01-20 10:19:06 -0800

    Adjust kext path for El Capitan

--- Installer/Scripts/Common.sh
+++ Installer/Scripts/Common.sh
@@ -3,7 +3,7 @@
 COMMON_DEFINED=yes
 
 KEXT_ID=com.protech.NoSleep
-KEXT_PATH=/System/Library/Extensions/NoSleep.kext
+KEXT_PATH=/Library/Extensions/NoSleep.kext
 PERF_PATH=/Library/PreferencePanes/NoSleep.prefPane
 HELPER_PATH=/Applications/Utilities/NoSleep.app
 LAUNCH_DAEMON_PATH=/Library/LaunchAgents/$KEXT_ID.plist
